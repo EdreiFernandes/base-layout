2e77db06d830de1f5acd2e92ada93dc6a2ca1eb3
Edrei <edreifernandes@gmail.com>
2020-09-14 13:13:31 -0300

    (uhmm) Criando comportamento de ligar e desligar divs

--- js/uhmm-behaviour.js
+++ js/uhmm-behaviour.js
@@ -2,22 +2,17 @@ var clicks = 0;
 
 function atualiza() {
   clicks++;
-  document.getElementById("top-text").innerHTML = clicks;
-  document.getElementById("top").hidden = false;
+  TurnOn("bottom", "left");
+  TurnOff("middle");
+}
 
-  //   if (clicks == 0) {
-  //     document.getElementById("middle").hidden = true;
-  //     document.getElementById("middle").className = "text-left";
-  //     document.getElementById("top").hidden = false;
-  //   } else if (clicks == 1) {
-  //     document.getElementById("top").hidden = true;
-  //     document.getElementById("top").className = "text-right";
-  //     document.getElementById("bottom").hidden = false;
-  //   } else if (clicks == 2) {
-  //     document.getElementById("bottom").hidden = true;
-  //     document.getElementById("bottom").className = "text-left";
-  //     document.getElementById("top").hidden = false;
-  //   } else {
-  //     document.getElementById("middle").hidden = false;
-  //   }
+function TurnOn(_div, _position) {
+  document.getElementById(_div + "-text").innerHTML = clicks;
+  var div = document.getElementById(_div);
+  div.className = "text-" + _position;
+  div.hidden = false;
+}
+
+function TurnOff(_div) {
+  document.getElementById(_div).hidden = true;
 }
