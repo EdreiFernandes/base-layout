dcdb16f4a65fae5c6c4781b3cbe3649ba14c32cb
Edrei <edreifernandes@gmail.com>
2020-09-14 13:51:51 -0300

    (Uhmm) Criando fluxo de cliques

--- js/uhmm-behaviour.js
+++ js/uhmm-behaviour.js
@@ -2,8 +2,34 @@ var clicks = 0;
 
 function atualiza() {
   clicks++;
-  TurnOn("bottom", "left");
-  TurnOff("middle");
+  if (clicks == 1) {
+    TurnOn("middle", "right");
+  } else if (clicks == 2) {
+    TurnOn("bottom", "right");
+    TurnOff("middle");
+  } else if (clicks == 3) {
+    TurnOn("top", "left");
+    TurnOff("bottom");
+  } else if (clicks == 4) {
+    TurnOn("bottom", "center");
+    TurnOff("top");
+  } else if (clicks == 5) {
+    TurnOn("top", "right");
+    TurnOff("bottom");
+  } else if (clicks == 6) {
+    TurnOn("middle", "left");
+    TurnOff("top");
+  } else if (clicks == 7) {
+    TurnOn("bottom", "left");
+    TurnOff("middle");
+  } else if (clicks == 8) {
+    TurnOn("top", "center");
+    TurnOff("bottom");
+  } else {
+    TurnOn("middle", "center");
+    TurnOff("top");
+    clicks = 0;
+  }
 }
 
 function TurnOn(_div, _position) {
